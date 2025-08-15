6f27bfe3d6e77bbc0775c1f44c8312617f98ad40
AndresCebay-ADSO <cebayy044@gmail.com>
2025-08-14 22:30:51 -0500

    Chat con la IA v1.2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+}); 
+
 Route::get('/test-db', function () {
     try {
         $tables = DB::select('SHOW TABLES');
